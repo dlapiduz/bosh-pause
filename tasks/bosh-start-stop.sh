45dc36a492c649bdfe9d96f66abd1bfae3ff5df9
Diego Lapiduz <dlapiduz@pivotal.io>
2017-03-03 15:31:06 -0600

    Fix task

--- tasks/bosh-start-stop.sh
+++ tasks/bosh-start-stop.sh
@@ -8,9 +8,24 @@ bosh-cli -e `cat om-bosh-creds/director_ip` \
 BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
 BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
 
+
+echo "Logging in to BOSH..."
+bosh-cli -e om-bosh log-in <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
+
 echo "Getting Deployments"
 
-bosh-cli --json -e om-bosh \
-    --client $BOSH_USERNAME \
-    --client-secret $BOSH_PASSWORD \
-    deployments | jq -r ".Tables[0].Rows[][0]"
+deployments=$(bosh-cli --json -e om-bosh \
+    deployments | jq -r ".Tables[0].Rows[][0]")
+
+for dep in $deployments; do
+  if [[ $dep != *"concourse"* ]]; then
+    if [[ $command == "start" ]]; then
+      bosh-cli -n -e om-bosh -d $dep start
+    else
+      bosh-cli -n -e om-bosh -d $dep stop
+    fi
+  fi
+done
